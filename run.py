12499a982d57bbfef3a1e178e114e8143a80b2e1
Sunil Prajapati <sunilp832@gmail.com>
2026-02-26 16:28:40 +0530

    edit run file

--- run.py
+++ run.py
@@ -1,12 +1,4 @@
-import eventlet
-eventlet.monkey_patch()   # MUST be first line
-
 from app import app, socketio
 
 if __name__ == "__main__":
-    socketio.run(
-        app,
-        host="127.0.0.1",
-        port=5000,
-        debug=True
-    )
+    socketio.run(app)
